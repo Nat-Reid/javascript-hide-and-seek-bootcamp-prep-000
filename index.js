804bbaeaf3abebbbfd3f7a8f436449179ace06a3
Nat Reid <nathanielreid@gmail.com>
2018-10-14 07:07:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+function nestedTarget(){
+  return document.querySelector('#nested .target');
+}
+function increaseRankBy(n){
+  var ranks = document.querySelectorAll('ul.ranked-list li');
+  for (let i=0; i<ranks.length; i++){
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n;
+  }
+}
+function deepestChild(){
+  var current = document.querySelector('#grand-node');
+  while (current.querySelector('div') != null){
+    current = current.querySelector('div');
+  }
+  return current;
+}
